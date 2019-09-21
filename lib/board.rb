aef3a049da42b4f320231d426a92e3df9906619c
punctual-enhancer-1584 <amal.jasen@gmail.com>
2019-09-21 17:44:16 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,5 @@
 # Define the variable board below.
-row=[" "] * 3
-board = row * 3 #this doesn't work in reverse (3 * board, bc that would try to coerce board into a number)
+#row=[" "] * 3
+#board = row * 3 #this doesn't work in reverse (3 * board, bc that would try to coerce board into a number)
+
+board = Array.new(9," ")
